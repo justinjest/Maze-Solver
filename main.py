cdc6827ef17cbb453f9525677c82291e0b29c51a
justinjest <jest.cayaban@gmail.com>
2024-09-16 10:32:13 +0900

    Added line draw functionality

--- main.py
+++ main.py
@@ -23,5 +23,27 @@ class Window():
     def close(self):
         self.running = False
 
+    def draw_line(self, line, fill_color):
+        line.draw(self.canvas, fill_color)
+
+class Point():
+    def __init__ (self, x, y):
+        self.x = x
+        self.y = y
+
+class Line():
+    def __init__(self, point_1, point_2):
+        self.point_1 = point_1
+        self.point_2 = point_2
+
+    def draw(self, canvas, fill_color):
+        canvas.create_line(self.point_1.x, self.point_1.y, 
+                           self.point_2.x, self.point_2.y, 
+                           fill = fill_color, width = 2)
+
 window = Window(800, 600)
+point1 = Point(10, 100)
+point2 = Point(20, 200)
+line = Line(point1, point2)
+window.draw_line(line, "red")
 window.wait_for_close()
